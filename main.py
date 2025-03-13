3c8e60dc9efe42542973d31a36ec53b34b8593dc
Kevin Bosirany ORLANDO <kevinbosirany@gmail.com>
2025-03-13 16:41:29 +0100

    Refactor transcribe_files function to accept model and additional parameters; update main execution to enable verbose output

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ def convertir(seconds):
   s = int(seconds % 60)
   return f"{h:02d}:{m:02d}:{s:02d}"
 
-def transcribe_files(project_path):
+def transcribe_files(project_path, model="turbo", **kwargs):
   import os
   import whisper
+  import tqdm
 
   input_dir = os.path.join("data", project_path, 'inputs')
   output_dir = os.path.join("data", project_path, 'outputs')
@@ -27,14 +28,14 @@ def transcribe_files(project_path):
   print(f"Transcribing files in {input_dir} and saving to {output_dir}")
   
   # Load model
-  model = whisper.load_model("turbo")
+  model = whisper.load_model(model)
 
   for input_file in os.listdir(input_dir):
     if input_file.endswith(".m4a"):
       input_file_path = os.path.join(input_dir, input_file)
       print(f"Transcribing {input_file_path}")
       output_file = os.path.join(output_dir, os.path.splitext(input_file)[0] + ".txt")
-      transcribed = model.transcribe(input_file_path)
+      transcribed = model.transcribe(input_file_path, **kwargs)
       # Enregistrement de la transcription
       with open(output_file , 'w', encoding='utf-8') as f:
         for segment in transcribed["segments"]:
@@ -49,6 +50,6 @@ def transcribe_files(project_path):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-  transcribe_files("test")
+  transcribe_files("test", verbose=True)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
